3572703ed6e6cd0d129d79832390b51d60109bb0
Evgeny <litun-07@mail.ru>
2021-02-24 23:33:21 +0300

    refactoring gendiff.py

--- difference_calculator/gendiff.py
+++ difference_calculator/gendiff.py
@@ -27,107 +27,101 @@ def diff(dict1, dict2):
     added_keys = sorted(list(dict2.keys() - dict1.keys()))
     deleted_keys = sorted(list(dict1.keys() - dict2.keys()))
     for key in common_keys:
-        if dict1[key] == dict2[key]:
-            if type(dict1[key]) is dict:
-                result.append({
-                    'name': key,
-                    'status': 'not changed',
-                    'type node': 'internal',
-                    'children': diff(dict1[key], dict2[key]),
-                })
-            else:
-                result.append({
-                    'name': key,
-                    'status': 'not changed',
-                    'type node': 'leaf',
-                    'data': dict1[key],
-                })
-        else:
-            if type(dict1[key]) is dict and type(dict2[key]) is dict:
-                result.append({
-                    'name': key,
-                    'status': 'not changed',
-                    'type node': 'internal',
-                    'children': diff(dict1[key], dict2[key]),
-                })
-            else:
-                if type(dict1[key]) is dict:
-                    result.append({
-                        'name': key,
-                        'status': 'before update',
-                        'type node': 'internal',
-                        'children': transform_dict(dict1[key]),
-                    })
-                else:
-                    result.append({
-                        'name': key,
-                        'status': 'before update',
-                        'type node': 'leaf',
-                        'data': dict1[key],
-                    })
-                if type(dict2[key]) is dict:
-                    result.append({
-                        'name': key,
-                        'status': 'after update',
-                        'type node': 'internal',
-                        'children': transform_dict(dict2[key]),
-                    })
-                else:
-                    result.append({
-                        'name': key,
-                        'status': 'after update',
-                        'type node': 'leaf',
-                        'data': dict2[key],
-                    })
+        process_common_keys(dict1, dict2, key, result)
     for key in added_keys:
-        if type(dict2[key]) is dict:
-            result.append({
-                'name': key,
-                'status': 'added',
-                'type node': 'internal',
-                'children': transform_dict(dict2[key]),
-            })
-        else:
-            result.append({
-                'name': key,
-                'status': 'added',
-                'type node': 'leaf',
-                'data': dict2[key],
-            })
+        process_added_keys(dict2, key, result)
     for key in deleted_keys:
-        if type(dict1[key]) is dict:
-            result.append({
-                'name': key,
-                'status': 'deleted',
-                'type node': 'internal',
-                'children': transform_dict(dict1[key]),
-            })
-        else:
-            result.append({
-                'name': key,
-                'status': 'deleted',
-                'type node': 'leaf',
-                'data': dict1[key],
-            })
+        process_deleted_keys(dict1, key, result)
     return result
 
 
+def process_common_keys(dict1, dict2, key, result):
+    if dict1[key] == dict2[key]:
+        if has_children(dict1[key]):
+            children = diff(dict1[key], dict2[key])
+            node = create_internal_node(key, 'not changed', children)
+            result.append(node)
+        else:
+            node = create_leaf_node(key, 'not changed', dict1[key])
+            result.append(node)
+    else:
+        if has_children(dict1[key]) and has_children(dict2[key]):
+            children = diff(dict1[key], dict2[key])
+            node = create_internal_node(key, 'not changed', children)
+            result.append(node)
+        else:
+            if has_children(dict1[key]):
+                children = transform_dict(dict1[key])
+                node = create_internal_node(key, 'before update', children)
+                result.append(node)
+            else:
+                node = create_leaf_node(key, 'before update', dict1[key])
+                result.append(node)
+            if has_children(dict2[key]):
+                children = transform_dict(dict2[key])
+                node = create_internal_node(key, 'after update', children)
+                result.append(node)
+            else:
+                node = create_leaf_node(key, 'after update', dict2[key])
+                result.append(node)
+
+
+def process_added_keys(dict2, key, result):
+    if has_children(dict2[key]):
+        children = transform_dict(dict2[key])
+        node = create_internal_node(key, 'added', children)
+        result.append(node)
+    else:
+        node = create_leaf_node(key, 'added', dict2[key])
+        result.append(node)
+
+
+def process_deleted_keys(dict1, key, result):
+    if has_children(dict1[key]):
+        children = transform_dict(dict1[key])
+        node = create_internal_node(key, 'deleted', children)
+        result.append(node)
+    else:
+        node = create_leaf_node(key, 'deleted', dict1[key])
+        result.append(node)
+
+
+def create_internal_node(name, status, children):
+    '''Return internal node.'''
+    return {
+        'name': name,
+        'status': status,
+        'type node': 'internal',
+        'children': children
+    }
+
+
+def create_leaf_node(name, status, data):
+    '''Return leaf node.'''
+    return {
+        'name': name,
+        'status': status,
+        'type node': 'leaf',
+        'data': data
+    }
+
+
+def has_children(value):
+    '''Checks if there are children in the dictionary.'''
+    if type(value) is dict:
+        return True
+    return False
+
+
 def transform_dict(dictionary):
-    """Transformation of the source dictionary. Return dictionary."""
+    """Transformation dictionaries to nodes. Returns a list of nodes."""
     result = []
     for key in dictionary.keys():
-        if type(dictionary[key]) is dict:
-            result.append({
-                'name': key,
-                'status': 'not changed',
-                'type node': 'internal',
-                'children': transform_dict(dictionary[key]),
-            })
+        if has_children(dictionary[key]):
+            children = transform_dict(dictionary[key])
+            node = create_internal_node(key, 'not changed', children)
+            result.append(node)
         else:
-            result.append({
-                'name': key,
-                'status': 'not changed',
-                'type node': 'leaf',
-                'data': dictionary[key],
-            })
+            node = create_leaf_node(key, 'not changed', dictionary[key])
+            result.append(node)
     return result
