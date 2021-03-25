6611022bf91190130ee5976243e493bf27983415
Evgeny <litun-07@mail.ru>
2021-03-25 23:28:09 +0300

    Correction of remarks made by mentor #3.4

--- tests/test_comprasion.py
+++ tests/test_comprasion.py
@@ -18,10 +18,10 @@ from pathlib import Path
 )
 def test_diff_stylish_json(file1, file2, right_answer, format_name):
     path_dict1 = Path(
-        Path(__file__).parent.absolute()/'fixtures'/file1)
+        Path(__file__).parent.absolute() / 'fixtures' / file1)
     path_dict2 = path_dict1.with_name(file2)
     path_right_answer = Path(
-        Path(__file__).parent.absolute()/'fixtures'/right_answer)
+        Path(__file__).parent.absolute() / 'fixtures' / right_answer)
     with open('{}'.format(path_right_answer)) as f:
         right_answer = f.read()[:-1]
     assert generate_diff(path_dict1, path_dict2, format_name) == right_answer
